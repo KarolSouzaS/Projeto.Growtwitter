a59e8c5a5d89cb0049262f9e9ac6bfad0dd65fd9
Karoline Souza <125404860+KarolSouzaS@users.noreply.github.com>
2026-03-29 22:23:30 -0300

    Refactor Login component layout and structure

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,21 +1,35 @@
 import "../styles/login.css"
-import { useDispatch } from 'react-redux'
-import { login } from '../store/slices/authSlice'
+import { useDispatch } from "react-redux"
+import { login } from "../store/slices/authSlice"
 
 export default function Login() {
   const dispatch = useDispatch()
 
   const handleLogin = () => {
-    dispatch(login('karol'))
+    dispatch(login("karol"))
   }
 
   return (
     <div className="container">
-      <div className="box">
-        <h1>Login</h1>
+      <div className="card">
+        <div className="logo">🐦</div>
+        <h1 className="title">Entrar no Growtwitter</h1>
+
+        <div className="input-group">
+          <input type="text" required placeholder=" " />
+          <label>Nome de usuário</label>
+        </div>
+
+        <div className="input-group">
+          <input type="password" required placeholder=" " />
+          <label>Senha</label>
+        </div>
+
         <button className="button" onClick={handleLogin}>
           Entrar
         </button>
+
+        <span className="link">Não tem uma conta? Cadastre-se</span>
       </div>
     </div>
   )
