884f30716a29c258b10701b5d9e6e57fbedb6191
Karoline Souza <125404860+KarolSouzaS@users.noreply.github.com>
2026-03-29 22:36:09 -0300

    Update logo SVG in Login component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -23,13 +23,14 @@ export default function Login() {
       <div className="card">
         
         {/* Logo */}
-        <div className="logo">
-          <svg viewBox="0 0 24 24" aria-hidden="true" className="logo-icon">
-            <g fill="#1d9bf0">
-              <path d="M18.244 2H21l-6.5 7.428L22 22h-6.828l-5.34-6.992L3.5 22H1l6.96-7.96L2 2h6.828l4.78 6.292L18.244 2Zm-2.39 18h1.89L8.27 4H6.22l9.634 16Z"/>
-            </g>
-          </svg>
-        </div>
+       <div className="logo">
+     <svg viewBox="0 0 24 24" className="logo-icon">
+     <path
+      fill="#1DA1F2"
+      d="M22.46 6c-.77.35-1.6.58-2.46.69a4.3 4.3 0 0 0 1.88-2.37 8.59 8.59 0 0 1-2.72 1.04 4.28 4.28 0 0 0-7.3 3.9A12.13 12.13 0 0 1 3.15 4.6a4.28 4.28 0 0 0 1.32 5.7 4.23 4.23 0 0 1-1.94-.54v.05a4.28 4.28 0 0 0 3.43 4.2 4.29 4.29 0 0 1-1.93.07 4.28 4.28 0 0 0 4 2.97A8.6 8.6 0 0 1 2 19.54a12.13 12.13 0 0 0 6.56 1.92c7.87 0 12.17-6.52 12.17-12.17 0-.19 0-.37-.01-.56A8.7 8.7 0 0 0 22.46 6z"
+        />
+      </svg>
+      </div>
 
         <h1 className="title">Entrar no Growtwitter</h1>
 
